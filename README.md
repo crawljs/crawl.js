c6a3edb197be370da79088c284f17ced0bd63946
Cédric Reginster <cedric.reginster@gmail.com>
2012-11-26 16:59:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Crawl.js - A simple web crawler written for node.js
+
+##Configuration
+Have a look at crawl.js
+
+##Usage
+node crawl.js
---
+++ crawl.js
@@ -0,0 +1,30 @@
+
+var NUMBER_OF_FETCHERS = 1
+  , HOST_TO_CRAWL = 'www.unibe.ch';
+
+var log     = require('./lib/logger')
+  , Feeder  = require('./lib/feeder')
+  , feeder  = new Feeder(HOST_TO_CRAWL)
+  , fetcher = require('./lib/fetcher')(NUMBER_OF_FETCHERS, feeder)
+  , start   = Date.now();
+
+function next() {
+
+  fetcher.get(feeder.dequeue(), function (err, url) {
+    if (err) {
+      log.error('could not crawl %s. error: %s', url, err);
+    } else if(!url) {
+      log.info('crawled %s in %s seconds', HOST_TO_CRAWL, (Date.now() - start)/1000);
+    } else {
+      next();
+    }
+  });
+
+}
+
+var start = Date.now();
+
+//start fetching
+for (var i = 0; i < NUMBER_OF_FETCHERS; i++) {
+  next();
+}
