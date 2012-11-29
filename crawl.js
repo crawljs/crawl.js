70c3107bc0659c74e22424db17fcb99ce1cad2a7
Cédric Reginster <cedric.reginster@gmail.com>
2012-11-29 17:56:46 +0100

    renamed domain to domainName. introduced events api to feeder. this allows us now to use more than one concurrent connection.

--- crawl.js
+++ crawl.js
@@ -1,34 +1,37 @@
 
-var NUMBER_OF_FETCHERS = 1;
+var MAX_CONN = 1;
 
 var log     = require('./lib/logger')
   , Feeder  = require('./lib/feeder')
   , url     = require('./lib/url')
-  , domain  = process.argv[2] || 'unine.ch'
-  , feeder  = new Feeder(domain)
-  , fetcher = require('./lib/fetcher')(NUMBER_OF_FETCHERS, feeder)
+  , domainName  = process.argv[2] || 'unine.ch'
+  , feeder  = new Feeder(domainName)
+  , fetcher = require('./lib/fetcher')(MAX_CONN, feeder)
   , start   = Date.now();
 
 function crawl() {
 
+  if (fetcher.busy()) {
+    //max number of concurrent connections reached. `MAX_CONN`
+    return;
+  }
+
   fetcher.get(feeder.dequeue(), function (err, url) {
     if (err) {
-      log.error('could not crawl %s. error: %s', url, err);
       crawl();
-    } else if(!url) {
-      log.info('crawled %s in %s seconds', domain, (Date.now() - start)/1000);
-    } else {
+    } else if (url) {
       crawl();
     }
   });
 
 }
 
-//TODO get seed from somewhere
-//TODO introduce event emitter api to feeder. (feeder.on('url', crawl...))
-feeder.enqueue(url.parse('http://www.' + feeder.domain + '/'));
+feeder.on('url', crawl);
 
-//start
-for (var i = 0; i < NUMBER_OF_FETCHERS; i++) {
-  crawl();
-}
+//TODO get seed from somewhere
+//triggers `url` event which starts the crawl
+feeder.enqueue(url.parse('http://www.' + feeder.domainName + '/'));
+
+process.on('exit', function () {
+  log.info('crawled %s in %s seconds', domainName, (Date.now() - start)/1000);
+});
