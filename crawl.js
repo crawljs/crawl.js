7c28cf47eaa3fbe504d0ee43d8633187b400745f
Cédric Reginster <cedric.reginster@gmail.com>
2013-03-12 16:13:22 +0100

    refactoring. introduced dispatcher (of urls).

--- crawl.js
+++ crawl.js
@@ -1,13 +1,20 @@
 
 var log     = require('./lib/logger')
-  , Queue   = require('./lib/queue')
-  , store   = require('./lib/store')
+  , Store   = require('./lib/store')
   , url     = require('./lib/url')
   , Fetcher = require('./lib/fetcher')
+  , Dispatcher = require('./lib/dispatcher')
   , conf    = require('./lib/config')()
   , queue
-  , fetcher
-  , store;
+  , store
+  , fetcher;
+
+if (typeof conf.block === 'undefined') {
+  throw new Error ('crawl.js needs to know which block it is responsible for! please specify `block` in configuration.');
+}
+
+store = Store.create('main');
+start();
 
 function printQueue() {
   process.stdout.write('Queue length: ' + queue.size() + '\r');
@@ -24,40 +31,44 @@ function crawl() {
 
   if (!url) {
     if (!fetcher.active()) {
-      //TODO ask for more
+      //TODO, restart
       return log.info('DONE!');
     }
   }
 
   fetcher.get(url, function () {
-    setTimeout(crawl, conf.interval);
     printQueue();
+    crawl();
   });
 
 }
 
-if (typeof conf.block === 'undefined') {
-  throw new Error ('crawl.js needs to know which block it is responsible for! please specify `block` in configuration.');
+function start () {
+  store.keys('urls.' + conf.block, function (err, urls) {
+    if (err) {
+      log.error('could not get urls. error: ' + err);
+    }
+    log.info('got ' + urls.length + ' new seed urls.');
+
+    //assemble crawler parts
+    var dispatcher = new Dispatcher();
+    fetcher = new Fetcher(dispatcher);
+
+    queue = dispatcher.getQueue();
+    queue.on('url', crawl);
+    queue.on('full', function () {
+      //Tell the dispatcher that we are busy
+      dispatcher.busy();
+    });
+
+    urls.forEach(function (urlString) {
+      //triggers `url` event which starts the crawl
+      dispatcher.dispatch(url.parse(urlString));
+    });
+
+  });
 }
 
-//init storage Engine and ask for our
-store = store.create('main');
-store.keys('urls.' + conf.block, function (err, urls) {
-  if (err) {
-    log.error('could not get urls. error: ' + err);
-  }
-  log.info('got ' + urls.length + ' new seed urls.');
-  //assemble crawler parts
-  queue = new Queue();
-  queue.on('url', crawl);
-  fetcher = new Fetcher(conf.fetchers, queue)
-
-  urls.forEach(function (urlString) {
-    //triggers `url` event which starts the crawl
-    queue.enqueue(url.parse(urlString));
-  });
-
-});
 
 process.on('SIGUSR1', function () {
   log.info('dump queue');
